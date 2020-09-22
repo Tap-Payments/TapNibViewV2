f7c519404fa68e157488007007871e7a7435de19
O Rabie <o.rabie@tap.company>
2020-09-22 10:06:27 +0200

    Upgraded TapNibView to Xcode 12+ compiler friendly code.

---
+++ TapNibViewV2/TapNibViewV2/TapNibViewV2.h
@@ -0,0 +1,18 @@
+//
+//  TapNibViewV2.h
+//  TapNibViewV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapNibViewV2.
+FOUNDATION_EXPORT double TapNibViewV2VersionNumber;
+
+//! Project version string for TapNibViewV2.
+FOUNDATION_EXPORT const unsigned char TapNibViewV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapNibViewV2/PublicHeader.h>
+
+
